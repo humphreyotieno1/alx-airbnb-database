7981de524d98c2b2bd37127834dad066c8e1c861
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-05-18 12:44:23 +0300

    Debug task 2

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,12 +7,15 @@ INNER JOIN Property p
 USING (property_id);
 
 -- Retrieve all properties and their reviews + properties that have no reviews
-SELECT p.property_id, p.name, p.location, r.rating, r.comment, u.first_name, u.last_name FROM
+SELECT p.property_id, p.name AS property_name, p.location, r.review_id, r.rating, r.comment, u.first_name AS reviewer_name
+FROM 
 Property p
-LEFT JOIN Review r
-USING (property_id)
-LEFT JOIN User u
-USING (user_id);
+LEFT JOIN 
+Review r ON p.property_id = r.property_id
+LEFT JOIN
+User u ON r.user_id = u.user_id
+ORDER BY 
+p.property_id, r.review_id;
 
 -- Retrieve all users and all bookings even if the user has no booking or a booking is not linked to a user
 SELECT u.user_id, u.first_name, u.last_name, u.email, u.phone_number, b.booking_id, b.property_id, b.status FROM
