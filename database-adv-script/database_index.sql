571083a753134e14f47826dd0e0695e5f86598e4
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-05-18 13:27:10 +0300

    Add explain analyze statement

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -13,3 +13,6 @@ CREATE INDEX idx_property_host_id ON Property(host_id);
 CREATE INDEX idx_property_location ON Property(location);
 CREATE INDEX idx_property_price ON Property(pricepernight);
 CREATE INDEX idx_property_location_price ON Property(location, pricepernight);
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE user_id = 'a1b2c3d4-e5f6-7890-abcd-ef1234567890';
